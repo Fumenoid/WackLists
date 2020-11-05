fdcfefe0d94e18472c7f61123882075d7a102933
fumenoid <fumenoid@localhost.localdomain>
2020-11-05 22:54:42 +0530

    Backend done

--- passcheck.py
+++ passcheck.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from forms import PasswordForm
+import searchpass
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = '15253fe70fb35e351631b47aeb9165e5' # secrets.token_hex(16)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    form = PasswordForm()
+    return render_template('index.html', form=form)
+
+@app.route("/result", methods=['POST'])
+def result():
+   if request.method == 'POST':
+        password = request.form['password']
+        result = searchpass.checkpass(password)
+        if result==True:
+            return render_template("result_present.html")
+        else:
+            return render_template("result_absent.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
