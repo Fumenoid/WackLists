f85022e74f020fae79a7e71e24bb854285e39aa5
fumenoid <fumenoid@localhost.localdomain>
2020-11-30 19:55:39 +0530

    Reverting

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class PasswordForm(FlaskForm):
-    password = PasswordField('Check for your Password', validators=[DataRequired()])
-    submit = SubmitField('Wacked ?')
--- searchpass.py
+++
@@ -1,9 +0,0 @@
-import mmap
-
-def checkpass(password):
-    with open('./templist.txt', 'rb', 0) as file, \
-         mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-        if s.find(bytes(password, 'utf-8')) != -1:
-            return True
-        else:
-            return False
